1a1b28a6058cf57dcf3b1f1003ba7a8f37cd747d
sukanikov <724454448@qq.com>
2020-10-15 20:13:00 +0800

    微信登录

--- guli-site/api/login.js
+++ guli-site/api/login.js
@@ -1,4 +1,5 @@
 import request from '~/utils/request'
+import cookie from 'js-cookie'
 
 export default {
   submitLogin(user) {
@@ -8,5 +9,18 @@ export default {
       method: 'post',
       data: user
     })
+  },
+
+  getLoginInfo() {
+    return request({
+      baseURL: 'http://localhost:8160',
+      url: '/api/ucenter/member/auth/get-login-info',
+      method: 'get',
+      // 添加自定义的header
+      headers: {
+        'token': cookie.get('guli_token')
+      }
+    })
   }
+
 }
--- guli-site/components/AppHeader.vue
+++ guli-site/components/AppHeader.vue
@@ -27,7 +27,7 @@
         </ul>
         <!-- / nav -->
         <ul class="h-r-login">
-          <li id="no-login">
+          <li v-if="!userInfo" id="no-login">
             <a href="/login" title="登录">
               <em class="icon18 login-icon">&nbsp;</em>
               <span class="vam ml5">登录</span>
@@ -38,25 +38,25 @@
             </a>
           </li>
           <!-- 注意undis将当前节点隐藏了 -->
-          <li id="is-login-one" class="mr10 undis">
+          <li v-if="userInfo" id="is-login-one" class="mr10 ">
             <a id="headerMsgCountId" href="#" title="消息">
               <em class="icon18 news-icon">&nbsp;</em>
             </a>
             <q class="red-point">&nbsp;</q>
           </li>
           <!-- 注意undis将当前节点隐藏了 -->
-          <li id="is-login-two" class="h-r-user undis">
+          <li v-if="userInfo" id="is-login-two" class="h-r-user ">
             <a href="/ucenter" title>
               <img
-                src="~/assets/img/avatar-boy.gif"
+                :src="userInfo.avatar"
+                :alt="userInfo.nickname"
                 width="30"
                 height="30"
                 class="vam picImg"
-                alt
               >
-              <span id="userName" class="vam disIb">登录的用户名</span>
+              <span id="userName" class="vam disIb">{{ userInfo.nickname }}</span>
             </a>
-            <a href="javascript:void(0)" title="退出" class="ml5">退出</a>
+            <a href="javascript:void(0)" title="退出" class="ml5" @click="logout()">退出</a>
           </li>
           <!-- /未登录显示第1 li；登录后显示第2，3 li -->
         </ul>
@@ -79,3 +79,53 @@
   </header>
   <!-- /公共头 -->
 </template>
+
+<script>
+import loginApi from '~/api/login'
+import cookie from 'js-cookie'
+
+export default {
+  data() {
+    return {
+      userInfo: null
+    }
+  },
+
+  created() {
+    this.getUserInfo()
+  },
+
+  methods: {
+    getUserInfo() {
+      // 判断客户端是否存在token
+      if (!cookie.get('guli_token')) {
+        return
+      }
+
+      console.log('获取用户信息')
+      // 从客户端获取用户信息:cookie
+      if (cookie.get('guli_user')) {
+        this.userInfo = JSON.parse(cookie.get('guli_user'))
+        return
+      }
+
+      // 从服务器端获取用户信息
+      loginApi.getLoginInfo().then(response => {
+        this.userInfo = response.data.userInfo
+
+        // 将用户信息存入cookie
+        cookie.set('guli_user', this.userInfo, {
+          domain: 'localhost',
+          expires: 1
+        })
+      })
+    },
+
+    logout() {
+      cookie.set('guli_token', '', { domain: 'localhost' })
+      cookie.set('guli_user', '', { domain: 'localhost' })
+      window.location.href = '/'
+    }
+  }
+}
+</script>
